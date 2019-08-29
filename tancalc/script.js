62e3968c57ccfac5a7b700ae0d1052049881dfbd
S-zebra <shimashimazebra13@gmail.com>
2019-08-29 23:01:40 +0900

    classを使用

--- tancalc/script.js
+++ tancalc/script.js
@@ -18,13 +18,13 @@ function addProduct() {
   pv.appendTo($("#productList"));
 
   let unitPriceField = pv.find("#unitPrice").attr("id", "unitPrice" + prodsLength);
-  unitPriceField.text(newProduct.getUnitPrice());
+  unitPriceField.text(newProduct.unitPrice);
 
   let amountField = pv.find("#amountField").attr("data-product-id", prodsLength);
   amountField.on("input", (e) => {
     let prodId = e.target.getAttribute("data-product-id");
     products[prodId].amount = parseFloat(e.target.value);
-    $("#unitPrice" + prodId).text(products[prodId].getUnitPrice());
+    $("#unitPrice" + prodId).text(products[prodId].unitPrice);
     highlightCheapest();
   });
   amountField.val(newProduct.amount.toString());
@@ -33,7 +33,7 @@ function addProduct() {
   priceField.on("input", (e) => {
     let prodId = e.target.getAttribute("data-product-id");
     products[prodId].price = parseFloat(e.target.value);
-    $("#unitPrice" + prodId).text(products[prodId].getUnitPrice());
+    $("#unitPrice" + prodId).text(products[prodId].unitPrice);
     highlightCheapest();
   });
   priceField.val(newProduct.price.toString());
@@ -42,9 +42,9 @@ function addProduct() {
 }
 
 function highlightCheapest() {
-  let minUnitPrice = Math.min.apply(null, products.map(p => p.getUnitPrice()));
+  let minUnitPrice = Math.min.apply(null, products.map(p => p.unitPrice));
   products.forEach(p => {
-    if (p.getUnitPrice() === minUnitPrice) {
+    if (p.unitPrice === minUnitPrice) {
       p.view.addClass("highlighted");
     } else {
       p.view.removeClass("highlighted");
@@ -52,11 +52,13 @@ function highlightCheapest() {
   });
 }
 
-function Product() {
-  this.price = 100;
-  this.amount = 100;
-  this.view = null;
-  this.getUnitPrice = function () {
+class Product {
+  constructor() {
+    this.price = 100;
+    this.amount = 100;
+    this.view = null;
+  }
+  get unitPrice() {
     return this.price / this.amount;
-  };
+  }
 }
