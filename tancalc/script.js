334cd3e32149c2d84f217042d7a94482ea086a2e
S-zebra <shimashimazebra13@gmail.com>
2021-10-04 22:38:13 +0900

    tancalc: minor updates

--- tancalc/script.js
+++ tancalc/script.js
@@ -1,9 +1,10 @@
 "use strict";
 let products = [];
 
-$(() => {
+$(function () {
   $("#addButton").on("click", addProduct);
   addProduct();
+  highlightCheapest();
 });
 
 function addProduct() {
@@ -21,34 +22,38 @@ function addProduct() {
   unitPriceField.text(newProduct.unitPrice);
 
   let amountField = pv.find("#amountField").attr("data-product-id", prodsLength);
-  amountField.on("input", (e) => {
-    let prodId = e.target.getAttribute("data-product-id");
-    products[prodId].amount = parseFloat(e.target.value);
-    $("#unitPrice" + prodId).text(products[prodId].unitPrice);
-    highlightCheapest();
-  });
+  amountField.on("input", updateUnitPrice);
   amountField.val(newProduct.amount.toString());
 
   let priceField = pv.find("#priceField").attr("data-product-id", prodsLength);
-  priceField.on("input", (e) => {
-    let prodId = e.target.getAttribute("data-product-id");
-    products[prodId].price = parseFloat(e.target.value);
-    $("#unitPrice" + prodId).text(products[prodId].unitPrice);
-    highlightCheapest();
-  });
+  priceField.on("input", updateUnitPrice);
   priceField.val(newProduct.price.toString());
 
   newProduct.view = pv;
   scrollTo(0, document.body.scrollHeight);
 }
 
+function updateUnitPrice(e) {
+  const prodId = e.target.getAttribute("data-product-id");
+  const newValue = parseFloat(e.target.value)
+  if (e.target.id.indexOf("price") == 0) {
+    products[prodId].price = newValue;
+  } else {
+    products[prodId].amount = newValue;
+  }
+  $("#unitPrice" + prodId).text(products[prodId].unitPrice.toString().slice(0, 4));
+  highlightCheapest();
+}
+
 function highlightCheapest() {
-  let minUnitPrice = Math.min.apply(null, products.map(p => p.unitPrice));
-  products.forEach(p => {
+  let minUnitPrice = Math.min.apply(null, products.map(function (p) { return p.unitPrice }));
+  products.forEach(function (p) {
     if (p.unitPrice === minUnitPrice) {
       p.view.addClass("highlighted");
+      p.view.find("#cheapestLabel").show();
     } else {
       p.view.removeClass("highlighted");
+      p.view.find("#cheapestLabel").hide();
     }
   });
 }
