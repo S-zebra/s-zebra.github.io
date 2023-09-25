d4a99fc89842a1dc91e70a6b45dc528a6a81fd24
S-zebra <shimashimazebra13@gmail.com>
2023-09-25 09:10:14 +0900

    removed MDB to speed up + going back to pure JS

--- tancalc/script.js
+++ tancalc/script.js
@@ -1,70 +1,86 @@
 "use strict";
-let products = [];
+const products = [];
 
-$(function () {
-  $("#addButton").on("click", addProduct);
+window.onload = function () {
+  document.getElementById("addButton").addEventListener("click", addProduct);
   addProduct();
   highlightCheapest();
-});
+};
+
+HTMLElement.prototype.addClass = function (cn) {
+  if (this.className.indexOf(cn) == -1) {
+    this.className += " " + cn;
+  }
+};
+
+HTMLElement.prototype.removeClass = function (cn) {
+  this.className = this.className.replace(new RegExp(" ?" + cn, "g"), "");
+};
 
 function addProduct() {
-  let newProduct = new Product();
+  const newProduct = new Product();
   products.push(newProduct);
 
-  let prodsLength = products.length - 1;
+  const prodsLength = products.length - 1;
 
-  let pv = $("#productItemBase").clone(true);
-  pv.attr("id", "product" + prodsLength);
+  const pv = document.getElementById("productItemBase").cloneNode(true);
+  pv.setAttribute("id", "product" + prodsLength);
   pv.removeClass("noshow");
-  pv.appendTo($("#productList"));
 
-  let unitPriceField = pv.find("#unitPrice").attr("id", "unitPrice" + prodsLength);
-  unitPriceField.text(newProduct.unitPrice);
+  document.getElementById("productList").appendChild(pv);
 
-  let amountField = pv.find("#amountField").attr("data-product-id", prodsLength);
-  amountField.on("input", updateUnitPrice);
-  amountField.val(newProduct.amount.toString());
+  const unitPriceLabel = pv.querySelector("#unitPrice");
+  unitPriceLabel.setAttribute("data-product-id", prodsLength);
+  unitPriceLabel.innerText = newProduct.unitPrice();
 
-  let priceField = pv.find("#priceField").attr("data-product-id", prodsLength);
-  priceField.on("input", updateUnitPrice);
-  priceField.val(newProduct.price.toString());
+  ["amount", "price"].forEach(function (f) {
+    const field = pv.querySelector("#" + f + "Field");
+    field.setAttribute("data-product-id", prodsLength);
+    field.addEventListener("input", updateUnitPrice);
+    field.value = newProduct[f].toString();
+  });
 
   newProduct.view = pv;
-  scrollTo(0, document.body.scrollHeight);
+  window.scrollTo(0, document.body.scrollHeight);
 }
 
 function updateUnitPrice(e) {
   const prodId = e.target.getAttribute("data-product-id");
-  const newValue = parseFloat(e.target.value)
+  const newValue = parseFloat(e.target.value);
   if (e.target.id.indexOf("price") == 0) {
     products[prodId].price = newValue;
   } else {
     products[prodId].amount = newValue;
   }
-  $("#unitPrice" + prodId).text(products[prodId].unitPrice.toString().slice(0, 4));
+  document.querySelector("span[data-product-id='" + prodId + "']").innerText =
+    products[prodId].unitPrice().toString().slice(0, 4);
   highlightCheapest();
 }
 
 function highlightCheapest() {
-  let minUnitPrice = Math.min.apply(null, products.map(function (p) { return p.unitPrice }));
+  const minUnitPrice = Math.min.apply(
+    null,
+    products.map(function (p) {
+      return p.unitPrice();
+    })
+  );
   products.forEach(function (p) {
-    if (p.unitPrice === minUnitPrice) {
+    if (p.unitPrice() == minUnitPrice) {
       p.view.addClass("highlighted");
-      p.view.find("#cheapestLabel").show();
+      p.view.querySelector("#cheapestLabel").removeClass("noshow");
     } else {
       p.view.removeClass("highlighted");
-      p.view.find("#cheapestLabel").hide();
+      p.view.querySelector("#cheapestLabel").addClass("noshow");
     }
   });
 }
 
-class Product {
-  constructor() {
-    this.price = 100;
-    this.amount = 100;
-    this.view = null;
-  }
-  get unitPrice() {
+// Fix: Compatibility for iOS 9.x
+function Product() {
+  this.price = 100;
+  this.amount = 100;
+  this.view = null;
+  this.unitPrice = function () {
     return this.price / this.amount;
-  }
+  };
 }
